6a1bb105149241775a19e329f7a1725afc83fc96
Brian Johnson (Linux Desktop) <bjthinks@gmail.com>
2015-02-12 17:39:36 -0800

    Add polynomial evaluation

--- experiment.cc
+++ experiment.cc
@@ -1,6 +1,27 @@
+#include <cstdlib>
+#include <cstdio>
+
 #include "mpoly.hh"
+
+#define should(b) should_((b), __LINE__)
+inline void should_(bool b, int line)
+{
+  if (!b) {
+    printf("Line %d: Something should be true but isn\'t.\n", line);
+    exit(1);
+  }
+}
 
 int main(int argc, char *argv[])
 {
+  // Polynomial x
+  MPoly<3> x = MPoly<3>::var(0);
+
+  // Vector (3,0,0)
+  Vector<3> c(0.0);
+  c[0] = 3.0;
+
+  should(x(c) == 3.0);
+
   return 0;
 }
--- mpoly.hh
+++ mpoly.hh
@@ -10,13 +10,61 @@
 template <int N>
 class Monomial
 {
+public:
+  Monomial()
+  {
+    for (int i = 0; i < N; ++i)
+      powers[i] = 0;
+  }
+
+  static Monomial<N> var(int n)
+  {
+    Monomial<N> x;
+    x.powers[n] = 1;
+    return x;
+  }
+
+  bool operator<(const Monomial<N> &rhs) const
+  {
+    for (int i = 0; i < N; ++i)
+      if (powers[i] < rhs.powers[i])
+        return true;
+    return false;
+  }
+
+  double operator()(const Vector<N> &p) const
+  {
+    double value = 1.0;
+    for (int i = 0; i < N; ++i)
+      value *= pow(p[i], powers[i]);
+    return value;
+  }
+
 private:
-  Vector<N> powers;
+  int powers[N];
 };
 
 template <int N>
 class MPoly
 {
+public:
+  static MPoly<N> var(int n)
+  {
+    Monomial<N> x = Monomial<N>::var(n);
+    MPoly<N> f;
+    f.terms[x] = 1.0;
+    return f;
+  }
+
+  double operator()(const Vector<N> &p) const
+  {
+    double value = 0.0;
+    typename std::map<Monomial<N>,double>::const_iterator i;
+    for (i = terms.begin(); i != terms.end(); ++i)
+      value += (i->second) * (i->first)(p);
+    return value;
+  }
+
 private:
   std::map<Monomial<N>,double> terms;
 };
