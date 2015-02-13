9060f4f778531e536631c8cfec24060bebf48927
Brian Johnson (Linux Desktop) <bjthinks@gmail.com>
2015-02-12 17:41:42 -0800

    Test more evaulation

--- experiment.cc
+++ experiment.cc
@@ -14,14 +14,26 @@ inline void should_(bool b, int line)
 
 int main(int argc, char *argv[])
 {
-  // Polynomial x
+  // Variables
   MPoly<3> x = MPoly<3>::var(0);
+  MPoly<3> y = MPoly<3>::var(1);
+  MPoly<3> z = MPoly<3>::var(2);
 
-  // Vector (3,0,0)
-  Vector<3> c(0.0);
-  c[0] = 3.0;
+  // Vectors a = (3,0,0), b = (0,5,0), c = (0,0,7)
+  Vector<3> a(0.0), b(0.0), c(0.0);
+  a[0] = 3.0;
+  b[1] = 5.0;
+  c[2] = 7.0;
 
-  should(x(c) == 3.0);
+  should(x(a) == 3.0);
+  should(x(b) == 0.0);
+  should(x(c) == 0.0);
+  should(y(a) == 0.0);
+  should(y(b) == 5.0);
+  should(y(c) == 0.0);
+  should(z(a) == 0.0);
+  should(z(b) == 0.0);
+  should(z(c) == 7.0);
 
   return 0;
 }
