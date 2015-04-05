6dc92779d37a4b13f03d1de11d18ec0298b00689
Brian Johnson (Linux Desktop) <bjthinks@gmail.com>
2015-04-05 10:21:59 -0700

    Construct all 20 basis functions

--- experiment.cc
+++ experiment.cc
@@ -243,7 +243,6 @@ void diag
  const Vector<3> &r, const string &r_name,
  const Vector<3> &s, const string &s_name)
 {
-  printf("Nonzero values and gradients:\n");
   show_value(f, f_name, p, p_name);
   show_value(f, f_name, q, q_name);
   show_value(f, f_name, r, r_name);
@@ -305,8 +304,6 @@ int main(int argc, char *argv[]) {
   MPoly<3> c = linear_indicator(r, p, q, s);
   MPoly<3> d = linear_indicator(s, p, q, r);
 
-  diag(a, "a", p, "p", q, "q", r, "r", s, "s");
-
   should(double_equal(a(p), 1.0));
   should(double_equal(a(q), 0.0));
   should(double_equal(a(r), 0.0));
@@ -323,6 +320,166 @@ int main(int argc, char *argv[]) {
   should(double_equal(d(q), 0.0));
   should(double_equal(d(r), 0.0));
   should(double_equal(d(s), 1.0));
+
+  MPoly<3> face_qrs = a*b*c*d * (b + c + d - 2.0 * a) * 27.0;
+  diag(face_qrs, "face_qrs", p, "p", q, "q", r, "r", s, "s");
+  MPoly<3> face_prs = a*b*c*d * (a + c + d - 2.0 * b) * 27.0;
+  diag(face_prs, "face_prs", p, "p", q, "q", r, "r", s, "s");
+  MPoly<3> face_pqs = a*b*c*d * (a + b + d - 2.0 * c) * 27.0;
+  diag(face_pqs, "face_pqs", p, "p", q, "q", r, "r", s, "s");
+  MPoly<3> face_pqr = a*b*c*d * (a + b + c - 2.0 * d) * 27.0;
+  diag(face_pqr, "face_pqr", p, "p", q, "q", r, "r", s, "s");
+
+  MPoly<3> edge_pq_to_r = 4.0 * a*b*c * (a + b - c)
+    + 16.0 / 81.0 * face_pqr
+    -  8.0 / 27.0 * face_pqs;
+  diag(edge_pq_to_r, "edge_pq_to_r", p, "p", q, "q", r, "r", s, "s");
+  MPoly<3> edge_pq_to_s = 4.0 * a*b*d * (a + b - d)
+    + 16.0 / 81.0 * face_pqs
+    -  8.0 / 27.0 * face_pqr;
+  diag(edge_pq_to_s, "edge_pq_to_s", p, "p", q, "q", r, "r", s, "s");
+  MPoly<3> edge_pr_to_q = 4.0 * a*c*b * (a + c - b)
+    + 16.0 / 81.0 * face_pqr
+    -  8.0 / 27.0 * face_prs;
+  diag(edge_pr_to_q, "edge_pr_to_q", p, "p", q, "q", r, "r", s, "s");
+  MPoly<3> edge_pr_to_s = 4.0 * a*c*d * (a + c - d)
+    + 16.0 / 81.0 * face_prs
+    -  8.0 / 27.0 * face_pqr;
+  diag(edge_pr_to_s, "edge_pr_to_s", p, "p", q, "q", r, "r", s, "s");
+  MPoly<3> edge_qr_to_p = 4.0 * b*c*a * (b + c - a)
+    + 16.0 / 81.0 * face_pqr
+    -  8.0 / 27.0 * face_qrs;
+  diag(edge_qr_to_p, "edge_qr_to_p", p, "p", q, "q", r, "r", s, "s");
+  MPoly<3> edge_qr_to_s = 4.0 * b*c*d * (b + c - d)
+    + 16.0 / 81.0 * face_qrs
+    -  8.0 / 27.0 * face_pqr;
+  diag(edge_qr_to_s, "edge_qr_to_s", p, "p", q, "q", r, "r", s, "s");
+  MPoly<3> edge_ps_to_q = 4.0 * a*d*b * (a + d - b)
+    + 16.0 / 81.0 * face_pqs
+    -  8.0 / 27.0 * face_prs;
+  diag(edge_ps_to_q, "edge_ps_to_q", p, "p", q, "q", r, "r", s, "s");
+  MPoly<3> edge_ps_to_r = 4.0 * a*d*c * (a + d - c)
+    + 16.0 / 81.0 * face_prs
+    -  8.0 / 27.0 * face_pqs;
+  diag(edge_ps_to_r, "edge_ps_to_r", p, "p", q, "q", r, "r", s, "s");
+  MPoly<3> edge_qs_to_p = 4.0 * b*d*a * (b + d - a)
+    + 16.0 / 81.0 * face_pqs
+    -  8.0 / 27.0 * face_qrs;
+  diag(edge_qs_to_p, "edge_qs_to_p", p, "p", q, "q", r, "r", s, "s");
+  MPoly<3> edge_qs_to_r = 4.0 * b*d*c * (b + d - c)
+    + 16.0 / 81.0 * face_qrs
+    -  8.0 / 27.0 * face_pqs;
+  diag(edge_qs_to_r, "edge_qs_to_r", p, "p", q, "q", r, "r", s, "s");
+  MPoly<3> edge_rs_to_p = 4.0 * d*c*a * (d + c - a)
+    + 16.0 / 81.0 * face_prs
+    -  8.0 / 27.0 * face_qrs;
+  diag(edge_rs_to_p, "edge_rs_to_p", p, "p", q, "q", r, "r", s, "s");
+  MPoly<3> edge_rs_to_q = 4.0 * c*d*b * (c + d - b)
+    + 16.0 / 81.0 * face_qrs
+    -  8.0 / 27.0 * face_prs;
+  diag(edge_rs_to_q, "edge_rs_to_q", p, "p", q, "q", r, "r", s, "s");
+
+  MPoly<3> vert_p_to_q = a*a*b
+    + 3.0 / 8.0 * (edge_pq_to_r + edge_pq_to_s)
+    - 1.0 / 4.0 * (edge_pr_to_q + edge_ps_to_q)
+    + 1.0 / 9.0 * (face_pqr + face_pqs - face_prs);
+  diag(vert_p_to_q, "vert_p_to_q", p, "p", q, "q", r, "r", s, "s");
+  MPoly<3> vert_p_to_r = a*a*c
+    + 3.0 / 8.0 * (edge_pr_to_q + edge_pr_to_s)
+    - 1.0 / 4.0 * (edge_pq_to_r + edge_ps_to_r)
+    + 1.0 / 9.0 * (face_pqr + face_prs - face_pqs);
+  diag(vert_p_to_r, "vert_p_to_r", p, "p", q, "q", r, "r", s, "s");
+  MPoly<3> vert_p_to_s = a*a*d
+    + 3.0 / 8.0 * (edge_ps_to_q + edge_ps_to_r)
+    - 1.0 / 4.0 * (edge_pq_to_s + edge_pr_to_s)
+    + 1.0 / 9.0 * (face_pqs + face_prs - face_pqr);
+  diag(vert_p_to_s, "vert_p_to_s", p, "p", q, "q", r, "r", s, "s");
+  MPoly<3> vert_q_to_p = b*b*a
+    + 3.0 / 8.0 * (edge_pq_to_r + edge_pq_to_s)
+    - 1.0 / 4.0 * (edge_qr_to_p + edge_qs_to_p)
+    + 1.0 / 9.0 * (face_pqr + face_pqs - face_qrs);
+  diag(vert_q_to_p, "vert_q_to_p", p, "p", q, "q", r, "r", s, "s");
+  MPoly<3> vert_q_to_r = b*b*c
+    + 3.0 / 8.0 * (edge_qr_to_p + edge_qr_to_s)
+    - 1.0 / 4.0 * (edge_pq_to_r + edge_qs_to_r)
+    + 1.0 / 9.0 * (face_pqr + face_qrs - face_pqs);
+  diag(vert_q_to_r, "vert_q_to_r", p, "p", q, "q", r, "r", s, "s");
+  MPoly<3> vert_q_to_s = b*b*d
+    + 3.0 / 8.0 * (edge_qs_to_p + edge_qs_to_r)
+    - 1.0 / 4.0 * (edge_pq_to_s + edge_qr_to_s)
+    + 1.0 / 9.0 * (face_pqs + face_qrs - face_pqr);
+  diag(vert_q_to_s, "vert_q_to_s", p, "p", q, "q", r, "r", s, "s");
+  MPoly<3> vert_r_to_p = c*c*a
+    + 3.0 / 8.0 * (edge_pr_to_q + edge_pr_to_s)
+    - 1.0 / 4.0 * (edge_qr_to_p + edge_rs_to_p)
+    + 1.0 / 9.0 * (face_pqr + face_prs - face_qrs);
+  diag(vert_r_to_p, "vert_r_to_p", p, "p", q, "q", r, "r", s, "s");
+  MPoly<3> vert_r_to_q = c*c*b
+    + 3.0 / 8.0 * (edge_qr_to_p + edge_qr_to_s)
+    - 1.0 / 4.0 * (edge_pr_to_q + edge_rs_to_q)
+    + 1.0 / 9.0 * (face_pqr + face_qrs - face_prs);
+  diag(vert_r_to_q, "vert_r_to_q", p, "p", q, "q", r, "r", s, "s");
+  MPoly<3> vert_r_to_s = c*c*d
+    + 3.0 / 8.0 * (edge_rs_to_p + edge_rs_to_q)
+    - 1.0 / 4.0 * (edge_pr_to_s + edge_qr_to_s)
+    + 1.0 / 9.0 * (face_prs + face_qrs - face_pqr);
+  diag(vert_r_to_s, "vert_r_to_s", p, "p", q, "q", r, "r", s, "s");
+  MPoly<3> vert_s_to_p = d*d*a
+    + 3.0 / 8.0 * (edge_ps_to_q + edge_ps_to_r)
+    - 1.0 / 4.0 * (edge_qs_to_p + edge_rs_to_p)
+    + 1.0 / 9.0 * (face_pqs + face_prs - face_qrs);
+  diag(vert_s_to_p, "vert_s_to_p", p, "p", q, "q", r, "r", s, "s");
+  MPoly<3> vert_s_to_q = d*d*b
+    + 3.0 / 8.0 * (edge_qs_to_p + edge_qs_to_r)
+    - 1.0 / 4.0 * (edge_ps_to_q + edge_rs_to_q)
+    + 1.0 / 9.0 * (face_pqs + face_qrs - face_prs);
+  diag(vert_s_to_q, "vert_s_to_q", p, "p", q, "q", r, "r", s, "s");
+  MPoly<3> vert_s_to_r = d*d*c
+    + 3.0 / 8.0 * (edge_rs_to_p + edge_rs_to_q)
+    - 1.0 / 4.0 * (edge_ps_to_r + edge_qs_to_r)
+    + 1.0 / 9.0 * (face_prs + face_qrs - face_pqs);
+  diag(vert_s_to_r, "vert_s_to_r", p, "p", q, "q", r, "r", s, "s");
+
+  MPoly<3> vert_p = a
+    + vert_p_to_q + vert_p_to_r + vert_p_to_s
+    - vert_q_to_p - vert_r_to_p - vert_s_to_p
+    + 1.0 / 2.0 * (edge_pq_to_r + edge_pq_to_s +
+                   edge_pr_to_q + edge_pr_to_s +
+                   edge_ps_to_q + edge_ps_to_r)
+    - edge_qr_to_p - edge_qs_to_p - edge_rs_to_p
+    + 1.0 / 3.0 * (face_pqr + face_pqs + face_prs)
+    - face_qrs;
+  diag(vert_p, "vert_p", p, "p", q, "q", r, "r", s, "s");
+  MPoly<3> vert_q = b
+    + vert_q_to_p + vert_q_to_r + vert_q_to_s
+    - vert_p_to_q - vert_r_to_q - vert_s_to_q
+    + 1.0 / 2.0 * (edge_pq_to_r + edge_pq_to_s +
+                   edge_qr_to_p + edge_qr_to_s +
+                   edge_qs_to_p + edge_qs_to_r)
+    - edge_pr_to_q - edge_ps_to_q - edge_rs_to_q
+    + 1.0 / 3.0 * (face_pqr + face_pqs + face_qrs)
+    - face_prs;
+  diag(vert_q, "vert_q", p, "p", q, "q", r, "r", s, "s");
+  MPoly<3> vert_r = c
+    + vert_r_to_p + vert_r_to_q + vert_r_to_s
+    - vert_p_to_r - vert_q_to_r - vert_s_to_r
+    + 1.0 / 2.0 * (edge_pr_to_q + edge_pr_to_s +
+                   edge_qr_to_p + edge_qr_to_s +
+                   edge_rs_to_p + edge_rs_to_q)
+    - edge_pq_to_r - edge_ps_to_r - edge_qs_to_r
+    + 1.0 / 3.0 * (face_pqr + face_prs + face_qrs)
+    - face_pqs;
+  diag(vert_r, "vert_r", p, "p", q, "q", r, "r", s, "s");
+  MPoly<3> vert_s = d
+    + vert_s_to_p + vert_s_to_q + vert_s_to_r
+    - vert_p_to_s - vert_q_to_s - vert_r_to_s
+    + 1.0 / 2.0 * (edge_ps_to_q + edge_ps_to_r +
+                   edge_qs_to_p + edge_qs_to_r +
+                   edge_rs_to_p + edge_rs_to_q)
+    - edge_pq_to_s - edge_pr_to_s - edge_qr_to_s
+    + 1.0 / 3.0 * (face_pqs + face_prs + face_qrs)
+    - face_pqr;
+  diag(vert_s, "vert_s", p, "p", q, "q", r, "r", s, "s");
 
   MPoly<3> e = linear_indicator(t, q, r, s);
   MPoly<3> f = linear_indicator(q, t, r, s);
